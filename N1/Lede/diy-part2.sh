1d14fc79cda8e7ca009a51929269e02688f684b0
ham0223 <126130711+ham0223@users.noreply.github.com>
2025-09-22 10:41:13 +0800

    Update diy-part2.sh

--- N1/Lede/diy-part2.sh
+++ N1/Lede/diy-part2.sh
@@ -28,8 +28,8 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 
 
 # 备份科学插件源：移除 openwrt feeds 自带的核心包
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
+#rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+#git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 # 更新 golang 1.25 版本
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
